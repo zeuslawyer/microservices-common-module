69a07b1eab6293c94a88cb431b9bb951fb9394bd
zeuslawyer <zubin.pratap@gmail.com>
2020-07-05 17:41:50 +1000

    update...

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,9 +1,11 @@
-// reference: https://expressjs.com/en/guide/error-handling.html
-
 import { Request, Response, NextFunction } from "express";
 import { CustomErrorBase } from "../Errors/CustomErrorBase";
 
 // error handling middleware accepts 4 args, with Error arg being the additional one
+/**
+ * reference: https://expressjs.com/en/guide/error-handling.html
+ * Handles all errors that arise in the server, and sends response back to client
+ */
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomErrorBase) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
